24f693c7ecf717e0d0dd856291548ebf24d6a9a9
Josha <28623436+JayBraker@users.noreply.github.com>
2022-08-11 11:27:18 +0200

    Parse moodle releases
    Fetches moodles releases, map date code to human friendly release number (ie 2007101591 -> 1.9.18)
    By default outputs entire available history.
    If date code is provided as argument, only outputs associated release.

---
+++ get-releases.py
@@ -0,0 +1,22 @@
+import sys
+import re
+import urllib.request
+import json
+
+build_no = sys.argv[1] if len(sys.argv) > 1 else "\d+"
+url = "https://docs.moodle.org/dev/Releases"
+#request = requests.get(url)
+request = urllib.request.Request(url)
+try:
+    response = urllib.request.urlopen(request)
+except Exception as e:
+    print(e)
+if not response.getcode() == 200:
+    print(response.getcode())
+    exit(1)
+html = response.read().decode()
+h_read_pattern = re.compile(r"<th>Moodle (?P<release>\d.+)\n<\/th>\n<td>\d+ \w+ \d\d\d\d\n<\/td>\n<td>(?P<build>{bn})".format(bn = build_no))
+ret = {}
+for match in h_read_pattern.findall(html):
+    ret[match[1]] = match[0]
+print(json.dumps(ret))
